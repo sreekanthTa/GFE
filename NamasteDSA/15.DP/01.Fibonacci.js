fc4ea018aaa0e393c20ad5e44b29b14546dd8ac5
sreekanthTa <sreekanthsreekantht970@gmail.com>
2025-09-22 07:48:59 +0530

    add fibonaccci

---
+++ NamasteDSA/15.DP/01.Fibonacci.js
@@ -0,0 +1,34 @@
+// const obj = {}
+
+// const fibonacci = (num) => {
+//     if(num<=1) return num
+//     if(obj[num]) return obj[num]
+//     console.log("fibinacci", num)
+//     const result =  fibonacci(num -1) + fibonacci(num - 2)
+//     obj[num] = result
+//     return obj[num]
+// }
+
+// console.log("fibonnaci series", fibonacci(5))
+
+
+const obj = {}
+
+const fibonacci = (num) => {
+     
+    let dp  = new Array(num + 1)
+    
+   previous1 = 1
+   previous2 = 0
+   current = 0
+    
+    for(let i = 2; i <= num; i++){
+        current = previous1 + previous2
+        previous2 = previous1
+        previous1 = current
+    }
+    
+    return  current
+}
+
+console.log("fibonnaci series", fibonacci(10))
