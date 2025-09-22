0aac0892745b5e9d6d89c0f2fa19215a775261c8
sreekanthTa <sreekanthsreekantht970@gmail.com>
2025-09-22 07:48:02 +0530

    p

--- NamasteDSA/13.BackTracking/09.Permutationsll.js
+++ NamasteDSA/13.BackTracking/09.Permutationsll.js
@@ -31,3 +31,18 @@ var permuteUnique = function(nums) {
     backTrack([]);
     return result; // <-- fix here
 };
+
+
+
+
+// Given a collection of numbers, nums, that might contain duplicates, return all possible unique permutations in any order.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,1,2]
+// Output:
+// [[1,1,2],
+//  [1,2,1],
+//  [2,1,1]]
